48b90f9485679ae122c88d65122d6bcee216d854
Nikita Khodakovsky <bednayadusha@gmail.com>
2023-12-05 22:32:02 +0200

    Server: Set up TypeORM

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { Module } from '@nestjs/common'
+
+import { dataSourceConfig } from './config/data-source.config'
 import { AppController } from './app.controller'
 
 @Module({
+	imports: [TypeOrmModule.forRoot(dataSourceConfig)],
 	controllers: [AppController]
 })
 export class AppModule {}
