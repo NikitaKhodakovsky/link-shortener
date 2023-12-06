13e10fc8d22c9a31abd6c635885296b10f7d4c84
Nikita Khodakovsky <bednayadusha@gmail.com>
2023-12-06 17:46:12 +0200

    Server: Add Swagger documentation for AuthController

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Session } from '@nestjs/common'
+import { BadRequestException, Body, Controller, HttpCode, HttpStatus, Post, Session } from '@nestjs/common'
+import { ApiException } from '@nanogiants/nestjs-swagger-api-exception-decorator'
 import { SessionData } from 'express-session'
+import { ApiTags } from '@nestjs/swagger'
 
+import { InvalidPasswordException, UsernameConflictException } from './auth.exception'
+import { UserNotFoundException } from '../user/user.exception'
 import { CredentialsDTO } from './credentials.dto'
 import { UserService } from '../user/user.service'
 import { AuthService } from './auth.service'
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
 	constructor(
@@ -13,6 +18,7 @@ export class AuthController {
 	) {}
 
 	@Post('register')
+	@ApiException(() => [BadRequestException, UsernameConflictException])
 	public async register(@Session() session: SessionData, @Body() { username, password }: CredentialsDTO) {
 		const userId = await this.authService.register(username, password)
 
@@ -23,6 +29,7 @@ export class AuthController {
 
 	@Post('login')
 	@HttpCode(HttpStatus.OK)
+	@ApiException(() => [BadRequestException, UserNotFoundException, InvalidPasswordException])
 	public async login(@Session() session: SessionData, @Body() { username, password }: CredentialsDTO) {
 		const userId = await this.authService.login(username, password)
 
