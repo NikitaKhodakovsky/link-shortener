d095a1d41c47040b55261769562cb7e6afb6ef7a
Nikita Khodakovsky <bednayadusha@gmail.com>
2023-12-12 22:29:40 +0200

    Client: Set up ReactQuery

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import React from 'react'
@@ -5,20 +7,26 @@ import React from 'react'
 import './sass/index.scss'
 
 import { AuthManager, AuthProvider } from './auth'
+
 import { App } from './components/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 const authManager = new AuthManager()
 
+const queryClient = new QueryClient()
+
 fetch('/auth/profile').then((res) => authManager.setAuth(res.ok))
 
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
-			<AuthProvider manager={authManager}>
-				<App />
-			</AuthProvider>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter basename={process.env.PUBLIC_URL}>
+				<AuthProvider manager={authManager}>
+					<App />
+				</AuthProvider>
+			</BrowserRouter>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	</React.StrictMode>
 )
