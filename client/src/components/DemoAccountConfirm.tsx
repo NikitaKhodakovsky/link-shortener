610b528991e84866045c6b72fd88c6e1338efd91
Nikita Khodakovsky <bednayadusha@gmail.com>
2023-12-13 20:47:33 +0200

    Client: Add appearance timeout for DemoAccountConfirm

--- client/src/components/DemoAccountConfirm.tsx
+++ client/src/components/DemoAccountConfirm.tsx
@@ -1,11 +1,19 @@
+import { useEffect } from 'react'
+
 import { useIsOpen } from '../hooks/useIsOpen'
 import { useAuth } from '../auth'
 
 import { Modal } from './Modal'
 
 export function DemoAccountConfirm() {
-	const [isOpen, close] = useIsOpen()
+	const [isOpen, close, open] = useIsOpen()
 	const { auth } = useAuth()
+
+	useEffect(() => {
+		const timeout = setTimeout(() => !auth && open(), 1500)
+
+		return () => clearTimeout(timeout)
+	}, [])
 
 	const createDemoAccount = async () => {
 		//TODO: disable button until request ends
@@ -14,7 +22,7 @@ export function DemoAccountConfirm() {
 	}
 
 	return (
-		<Modal title="For strangers" isOpen={!auth && isOpen} closeHandler={close}>
+		<Modal title="For strangers" isOpen={isOpen} closeHandler={close}>
 			<p>Create a demo account with demo data without registration?</p>
 			<div className="actions">
 				<button className="button transparent" onClick={close}>
