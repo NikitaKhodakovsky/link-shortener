2f5961a7fedee42adaa2d358d542bda953fad1fd
Nikita Khodakovsky <bednayadusha@gmail.com>
2023-12-18 19:43:30 +0200

    Server: Fix production Dockerfile

--- server/src/config/data-source.config.ts
+++ server/src/config/data-source.config.ts
@@ -2,6 +2,9 @@ import { DataSource, DataSourceOptions } from 'typeorm'
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 
 import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME } from './env'
+import { Click } from '../modules/click/click.entity'
+import { User } from '../modules/user/user.entity'
+import { Link } from '../modules/link/link.entity'
 
 export const dataSourceOptions: DataSourceOptions = {
 	type: 'postgres',
@@ -9,12 +12,12 @@ export const dataSourceOptions: DataSourceOptions = {
 	port: DB_PORT,
 	username: DB_USERNAME,
 	password: DB_PASSWORD,
-	database: DB_DATABASE
+	database: DB_DATABASE,
+	entities: [User, Link, Click]
 }
 
 export const dataSource = new DataSource(dataSourceOptions)
 
 export const typeOrmModuleOptions: TypeOrmModuleOptions = {
-	...dataSourceOptions,
-	autoLoadEntities: true
+	...dataSourceOptions
 }
