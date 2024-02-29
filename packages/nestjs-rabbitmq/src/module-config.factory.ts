37f94f752eb8cb79c3ccced2081d77cce6569bea
Nikita Khodakovsky <bednayadusha@gmail.com>
2024-02-29 05:31:36 +0200

    Project: Change default RabbitMQ prefetch count

--- packages/nestjs-rabbitmq/src/module-config.factory.ts
+++ packages/nestjs-rabbitmq/src/module-config.factory.ts
@@ -1,12 +1,20 @@
 import { MessageHandlerErrorBehavior, RabbitMQConfig, defaultNackErrorHandler } from '@golevelup/nestjs-rabbitmq'
 import { DLXQueue, DeadLetterExchange } from '@app/dlx-rabbitmq-contract'
 
-export function createConfig({ queues, exchanges, defaultRpcErrorHandler, defaultSubscribeErrorBehavior, ...other }: RabbitMQConfig) {
+export function createModuleConfig({
+	queues,
+	exchanges,
+	defaultRpcErrorHandler,
+	defaultSubscribeErrorBehavior,
+	prefetchCount,
+	...other
+}: RabbitMQConfig) {
 	return {
 		defaultRpcErrorHandler: defaultRpcErrorHandler ?? defaultNackErrorHandler,
 		defaultSubscribeErrorBehavior: defaultSubscribeErrorBehavior ?? MessageHandlerErrorBehavior.NACK,
 		exchanges: [DeadLetterExchange, ...(exchanges ?? [])],
 		queues: [DLXQueue, ...(queues ?? [])],
+		prefetchCount: prefetchCount ?? 100,
 		...other
 	}
 }
--- packages/nestjs-rabbitmq/src/rabbitmq.module.ts
+++ packages/nestjs-rabbitmq/src/rabbitmq.module.ts
@@ -1,7 +1,7 @@
 import { RabbitMQConfig as XRabbitMQConfig, RabbitMQModule as XRabbitMQModule } from '@golevelup/nestjs-rabbitmq'
 import { DynamicModule, Module } from '@nestjs/common'
 
-import { createConfig } from './config.factory'
+import { createModuleConfig } from './module-config.factory'
 
 export interface RabbitMQConfig extends XRabbitMQConfig {
 	global?: boolean
@@ -12,7 +12,7 @@ export class RabbitMQModule {
 	public static register({ global, ...config }: RabbitMQConfig): DynamicModule {
 		return {
 			module: RabbitMQModule,
-			imports: [XRabbitMQModule.forRoot(XRabbitMQModule, createConfig(config))],
+			imports: [XRabbitMQModule.forRoot(XRabbitMQModule, createModuleConfig(config))],
 			exports: [XRabbitMQModule],
 			global
 		}
